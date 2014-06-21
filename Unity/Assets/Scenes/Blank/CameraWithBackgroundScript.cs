737b8d15454bd26f1e3467e42ec53794dadfcbdf
Vasily M <migynov@gmail.com>
2014-06-21 16:00:18 +0400

    Начал работать над кликом по  объекту

---
+++ Unity/Assets/Scenes/Blank/CameraWithBackgroundScript.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraWithBackgroundScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnMouseUpAsButton()
+	{
+		// Кликнули ли на кликаемый объект - передаем управление ему
+
+
+		// Кликнули ли на пол - двигаемся туда
+	}
+}
